d6af56d2cc2c2e6eeffc0a39d37257526a9f37cc
MD Shoriful Islam <cmshohelrana50@gmail.com>
2026-05-31 14:22:47 +0600

    Update AddToCartButton.tsx

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -37,7 +37,6 @@ const AddToCartButton = memo(({ product, className, selectedOptions, displayPric
   const itemCount = getItemCount(product?._id || '');
   const isOutOfStock = product?.stock === 0;
 
-  // This ensures that the component only renders on the client-side
   // Preventing hydration errors due to server/client mismatch
   useEffect(() => {
     setIsClient(true);
