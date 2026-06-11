3a61e2550587e97c8d2ecdca6b683049874b07c3
shoriful-dev <cmshohelrana50@gmail.com>
2026-06-11 22:31:21 +0600

    fix css

--- components/common/ImageView.tsx
+++ components/common/ImageView.tsx
@@ -36,7 +36,7 @@ const ImageView = ({ images = [], isStock }: Props) => {
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ duration: 0.5 }}
-          className="w-full max-h-[550px] min-h-[450px] border border-gofarm-black/10 rounded-md group overflow-hidden"
+          className="w-full max-h-137.5 min-h-112.5 border border-gofarm-black/10 rounded-md group overflow-hidden"
         >
           <Image
             src={urlFor(active).url()}
@@ -44,7 +44,7 @@ const ImageView = ({ images = [], isStock }: Props) => {
             width={700}
             height={700}
             priority
-            className={`w-full h-96 max-h-[550px] min-h-[500px] object-contain group-hover:scale-110 hoverEffect rounded-md ${
+            className={`w-full h-96 max-h-137.5 min-h-125 object-contain group-hover:scale-110 hoverEffect rounded-md ${
               isStock === 0 ? "opacity-50" : ""
             }`}
           />
